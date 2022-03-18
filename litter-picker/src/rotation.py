3b59df182425f2473db5b9a076238cc60fa9d321
vbelkina <vbelkina95@gmail.com>
2022-03-18 19:15:54 +0000

    edited patrol and rotation to match new master implementation

--- litter-picker/src/rotation.py
+++ litter-picker/src/rotation.py
@@ -5,39 +5,37 @@ import constants
 from geometry_msgs.msg import Twist
 import math
 
-state_pub = rospy.Publisher(constants.STATE_TOPIC_NAME, Int32, queue_size=1)
-cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
-
-
-def get_state(msg: Int32):
+def get_state(msg):
     global state
     state = msg.data
 
+def odom_cb(msg):
+    global position 
+    position = msg.data 
+
+def rotation_cb(msg):
+    global rotation_goal
+    rotation_goal = msg.data
 
 # Main program starts here
 if __name__ == '__main__':
-    state = constants.GO_TO_NEXT_WAYPOINT
     rospy.init_node('rotation')
-    state_sub = rospy.Subscriber(constants.STATE_TOPIC_NAME, Int32, get_state)
-    time_started_rotation = rospy.Time.now()
+    state_pub = rospy.Subscriber(constants.STATE_TOPIC_NAME, Int32, get_state)
+    odom_sub = rospy.Subscriber('odom', Odometry, odom_cb)
+    rotation_sub = rospy.Subscriber('rotation/goal', Float32, rotation_cb)
+    cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
     twist = Twist()
 
     while not rospy.is_shutdown():
-        if state == constants.REACHED_WAYPOINT:
-            print("STATE in if", state)
-            print("reached waypoint")
-            time_started_rotation = rospy.Time.now()
-            state_pub.publish(constants.ROTATING)
-        elif state == constants.ROTATING:
-            while (rospy.Time.now() - time_started_rotation).to_sec() <= 4 and not rospy.is_shutdown():
-                print("STATE in while", state)
-                print("inside rotation loop")
-                twist.angular.z = math.pi/2
-                cmd_vel_pub.publish(twist)
-            state_pub.publish(constants.GO_TO_NEXT_WAYPOINT)
-            print("STATE after while", state)
-            twist.angular.z = 0
-            cmd_vel_pub.publish(twist)
-            print("TWIST: ", twist.angular.z)
-            print("end rotating")
+        z = position.angular.z 
+        
+        if (rotation_goal - z) != 0:
+            twist.angular.z = 0.2 
+            state_pub.publish(0)
+        else:  
+            twist.angular.z = 0 
+            state_pub.publish(1)
+            
+        cmd_vel_pub.publish(twist)
             
