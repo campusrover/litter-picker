6dffa89f71d9a4faef32ca8f4f44c1e0383dcc3d
Peter <peterzhao204@gmail.com>
2022-05-03 19:02:21 -0400

    added vision node that publishes relavent information

--- litter_picker/src/vision.py
+++ litter_picker/src/vision.py
@@ -1,9 +1,88 @@
+import math
+
 import rospy
+from cv_bridge import CvBridge
+from darknet_ros_msgs.msg import BoundingBoxes
+from sensor_msgs.msg import CompressedImage
+from litter_picker.msg import Trash
+
+import topics
+from utils import get_first_bonding_box
 
 
 class Vision:
-    pass
+    def __init__(self):
+        self.image_sub = rospy.Subscriber(topics.IMAGE_TOPIC, CompressedImage,
+                                          self.get_width_and_height_cb())
+        self.box_sub = rospy.Subscriber(topics.BOUNDING_BOXES, BoundingBoxes,
+                                        self.get_bonding_box_err_cb())
+        self.trash_pub = rospy.Publisher(topics.TRASH_TOPIC, Trash)
+
+        self.image_wh = None
+        self.bounding_box_err = None
+        self.bounding_box_coord = None
+        self.distance_to_box = None
+
+    def get_bonding_box_err_cb(self):
+
+        def cb(msg: BoundingBoxes):
+            if self.image_wh is None:
+                rospy.logwarn("unable to get the image's shape")
+            else:
+                _, w, _ = self.image_wh
+                box = get_first_bonding_box(msg.bounding_boxes)
+                if box is None:
+                    self.bonding_box_err = None
+                else:
+                    self.bounding_box_coord = (
+                        (box.xmin + box.xmax) / 2,
+                        (box.ymin + box.ymax) / 2
+                    )
+                    center = (box.xmin + box.xmax) / 2
+                    image_center = w / 2
+                    self.bonding_box_err = abs(image_center - center)
+
+        return cb
+
+    def get_width_and_height_cb(self):
+
+        def cb(msg: CompressedImage):
+            # infer once during the lifetime of the object
+            if self.image_wh is None:
+                bridge = CvBridge()
+                image = bridge.compressed_imgmsg_to_cv2(msg)
+                self.image_wh = image.shape
+
+        return cb
+
+    def _depth_cb(self):
+
+        def depth_cb(msg):
+            if self.bounding_box_coord is None:
+                self.distance_to_box = None
+            else:
+                bridge = CvBridge()
+                image = bridge.compressed_imgmsg_to_cv2(msg)
+                box_x, box_y = self.bounding_box_coord
+                distance = image[box_x][box_y]
+                self.distance_to_box = 0 if math.isnan(distance) else distance
+
+        return depth_cb
+
+    def publish_data(self):
+        msg = Trash()
+        msg.has_trash = self.bounding_box_coord is None
+        msg.dist_to_trash = self.distance_to_box
+        msg.err_to_center = self.bounding_box_err
+
+        self.trash_pub.publish()
 
 
 if __name__ == '__main__':
     rospy.init_node("vision_processor")
+    vision_node = Vision()
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        vision_node.publish_data()
+        rate.sleep()
