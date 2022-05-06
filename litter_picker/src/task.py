fd8841fb20da07eb4193371d6035bac4b38215be
Peter <peterzhao204@gmail.com>
2022-05-06 18:56:07 -0400

    added more documentation

--- litter_picker/src/task.py
+++ litter_picker/src/task.py
@@ -4,7 +4,11 @@ from master import LitterPickerState
 
 
 class Task:
-
+    """
+    An abstract class that represents a single task to be executed at a certain point. The subclass
+    should implement the start() method, which begins the execution of the task, as well as the next()
+    method which returns the next task to be executed give certain condition passed into self.state
+    """
     def __init__(self, state: LitterPickerState):
         """
         constructor fo the task
@@ -13,8 +17,15 @@ class Task:
         instance doesn't remember anything, it provides information for the task needed to perform
         correctly at the current moment.
         """
+
+        # the current litter picker state/the state after the last task has been executed
         self.state = state
+
+        # whether the task was executed successfully or not
         self.has_succeeded = False
+
+        # the default rate to sleep in the while loop that continue publishes message to prevent
+        # the ros node from overheating.
         self.rate = rospy.Rate(10)
 
     def start(self):
