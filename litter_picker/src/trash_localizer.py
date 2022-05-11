e585f1448bd78402df57fea0711f9e71780ab780
Peter <peterzhao204@gmail.com>
2022-05-10 23:40:47 -0400

    make sure litter picker directly faces trash before trapping it

--- litter_picker/src/trash_localizer.py
+++ litter_picker/src/trash_localizer.py
@@ -6,6 +6,9 @@ from litter_picker.msg import Trash
 import topics
 
 # the number of seconds to move the litter picker forward to trap the trash
+APPROACH_TRASH_VELOCITY = 0.2
+ERR_TO_CENTER_TOLERANCE = 60
+TRAP_VELOCITY = 0.2
 MAX_RETRY_LOST_BOUNDING_BOXES = 3
 NUMBER_OF_SECONDS_TO_TRAP = 2
 
@@ -42,10 +45,11 @@ class TrashLocalizerTask(Task):
 
         # twist message for moving the litter picker toward the trash
         self.vel = Twist()
-        self.vel.linear.x = 0.2
+        self.vel.linear.x = APPROACH_TRASH_VELOCITY
 
         # twist message for stopping the robot
         self.stop = Twist()
+        self.stop.linear.x = 0
 
     def _trash_cb(self):
         """
@@ -69,7 +73,9 @@ class TrashLocalizerTask(Task):
         it will try to move forward a bit to trap the trash
         """
         rospy.loginfo("[Trash localizer: ] ready to move toward the trash")
-        while not rospy.is_shutdown() and self.has_box and not self.is_close_enough:
+        while not rospy.is_shutdown() and self.has_box and (
+                not self.is_close_enough or abs(self.err_to_center) > ERR_TO_CENTER_TOLERANCE):
+            self.vel.linear.x = 0 if self.is_close_enough else APPROACH_TRASH_VELOCITY
             self.vel.angular.z = -self.err_to_center / 1500
             self.cmd_vel_pub.publish(self.vel)
             rospy.loginfo("[Trash localizer:] current velocity = {}, angular speed = {}".format(
@@ -94,6 +100,7 @@ class TrashLocalizerTask(Task):
         starting_time = rospy.Time.now().to_sec()
         while not rospy.is_shutdown(
         ) and rospy.Time.now().to_sec() - starting_time < NUMBER_OF_SECONDS_TO_TRAP:
+            self.vel.linear.x = TRAP_VELOCITY
             self.vel.angular.z = 0
             self.cmd_vel_pub.publish(self.vel)
             self.rate.sleep()
